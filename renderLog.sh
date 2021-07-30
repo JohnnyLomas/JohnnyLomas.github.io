a7435ae265b2ae3c162e9fcd421c544a76e8f9eb
John Lomas <jslomas@ucdavis.edu>
2021-07-30 10:33:23 -0700

    pat

--- renderLog.sh
+++ renderLog.sh
@@ -1,4 +1,5 @@
-
+#!/bin/bash
+cd /Users/jslomas/Documents/JohnnyLomas.github.io/
 Rscript -e "rmarkdown::render('Training_Log.Rmd', 'html_document')"
 cp Training_Log.html index.html
 git add index.html
