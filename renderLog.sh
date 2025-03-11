73ae31b82bc20f6891cb19328e4f6336317c71d9
John Lomas <jslomas@ucdavis.edu>
2025-03-11 13:57:16 -0700

    Updating website to personal portfolio

--- renderLog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /Users/jslomas/Documents/JohnnyLomas.github.io/
-Rscript -e "rmarkdown::render('Training_Log.Rmd', 'html_document')"
-cp Training_Log.html index.html
-git add index.html
-git commit -m "committing new training log"
-git push
